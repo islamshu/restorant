7618106cc359f4eb26940cc1f52e92a7c1e7e83c
islamshu12 <islamshu12@gmail.com>
2023-06-18 13:20:04 +0300

    edit

--- resources/views/dashboard/orders/index.blade.php
+++ resources/views/dashboard/orders/index.blade.php
@@ -214,13 +214,13 @@
                     var startTime = $(this).data('time-start');
                     var diff = new Date(startTime);
                     // alert(diff);
-                    // var current = new Date();
-                    var d = new Date();
-                    var local = d.getTime();
-                    var offset = d.getTimezoneOffset() * (60 * 1000);
+                    var current = new Date();
+                    // var d = new Date();
+                    // var local = d.getTime();
+                    // var offset = d.getTimezoneOffset() * (60 * 1000);
                     
-                    var utc = new Date(local + offset);
-                    var current = new Date(utc.getTime() + ( 2+60 * 60 * 1000));
+                    // var utc = new Date(local + offset);
+                    // var current = new Date(utc.getTime() + ( 2+60 * 60 * 1000));
                     // alert(current);
 
                     $(this).html(calculateTimeDifference(diff, current));
